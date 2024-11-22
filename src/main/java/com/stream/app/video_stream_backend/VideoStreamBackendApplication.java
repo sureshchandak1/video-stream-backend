f93d2d912806edd7934a2b959adcc0724c3b24d2
SSureshC <sureshchandak1@gmail.com>
2024-11-22 19:20:47 +0530

    Project Setup

---
+++ src/main/java/com/stream/app/video_stream_backend/VideoStreamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stream.app.video_stream_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stream/app/video_stream_backend/VideoStreamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.app.video_stream_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
