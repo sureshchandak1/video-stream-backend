150b3d480bf7b2b0ec02901f3a3011becd5fb9a8
SSureshC <sureshchandak1@gmail.com>
2024-11-23 12:37:02 +0530

    Create folder and save video inside

--- src/main/java/com/stream/app/controllers/VideoController.java
+++ src/main/java/com/stream/app/controllers/VideoController.java
@@ -1,5 +1,8 @@
 package com.stream.app.controllers;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.stream.app.entities.Video;
+import com.stream.app.playload.CustomMessage;
 import com.stream.app.services.VideoService;
 
 @RestController
@@ -20,11 +25,27 @@ public class VideoController {
     }
 
     @PostMapping
-    public ResponseEntity<CustomMessage> create(
+    public ResponseEntity<?> create(
         @RequestParam("file") MultipartFile file,
         @RequestParam("title") String title,
         @RequestParam("description") String description
     ) {
+
+        Video video = new Video();
+        video.setVideoId(UUID.randomUUID().toString());
+        video.setTitle(title);
+        video.setDescription(description);
+
+        Video savedVideo = mVideoService.save(video, file);
+
+        if (savedVideo != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(video);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                    CustomMessage.builder().message("Video not uploaded").success(false).build()
+                );
+        }
 
     }
 
--- src/main/java/com/stream/app/services/impl/VideoServiceImpl.java
+++ src/main/java/com/stream/app/services/impl/VideoServiceImpl.java
@@ -1,23 +1,42 @@
 package com.stream.app.services.impl;
 
+import java.io.File;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.stream.app.entities.Video;
+import com.stream.app.repositories.VideoRepository;
 import com.stream.app.services.VideoService;
+
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class VideoServiceImpl implements VideoService {
 
     @Value("${files.video}")
     String DIR;
+
+    @Autowired
+    private VideoRepository mVideoRepository;
+
+    @PostConstruct
+    public void init() {
+
+        File file = new File(DIR);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
 
     @Override
     public Video save(Video video, MultipartFile file) {
@@ -33,20 +52,21 @@ public class VideoServiceImpl implements VideoService {
 
             Path path = Paths.get(cleanFolder, clearFileName);
 
-            System.err.println(path);
-
             // copy file to the folder
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
 
             // video meta data
+            video.setContentType(contentType);
+            video.setFilePath(path.toString());
 
             // metadata save
+            return mVideoRepository.save(video);
 
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
         
-
-        return null;
     }
 
     @Override
